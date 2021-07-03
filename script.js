2c566d8e1bdb36dd22899d3b1c8a802adfa8c406
antispoonfanatic <allanrajcrp@gmail.com>
2021-07-04 01:01:06 +0530

    June 3 changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".navlinker");
+const navLinks = document.querySelectorAll(".navlink");
 
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
